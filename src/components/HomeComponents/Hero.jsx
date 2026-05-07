bc7844a559681c78131367a9a1941c30175fdf23
Nantanat-Poyomratanasin <nantanat.poyomratanasin@gmail.com>
2026-05-07 19:29:14 +0700

    update hero section

--- src/components/HomeComponents/Hero.jsx
+++ src/components/HomeComponents/Hero.jsx
@@ -1,48 +1,72 @@
 import * as React from "react";
+import { useRef } from "react";
 
 export default function Hero() {
+  const scrollRef = useRef(null);
   return (
-    <section className="h-[500px] pl-[100px] py-[40px] bg-[#ECE0DC] font-['Cormorant_Garamond'] flex flex-row">
-      {/* LEFT TEXT */}
-      <div className="w-[240px] mr-[48px] [text-wrap:pretty]">
-        <h1 className="font-extrabold text-6xl">
-          Trending <br /> Books
-        </h1>
-        <p className="mt-2 text-2xl font-semibold">
+    <section className="px-10 sm:px-10 lg:px-[100px] py-8 bg-[#ECE0DC] font-['Cormorant_Garamond'] flex flex-col sm:flex-row gap-5">
+      <div className="[text-wrap:pretty]">
+        <h1 className="font-extrabold text-2xl sm:text-3xl">Trending Books</h1>
+        <p className="mt-2 text-lg sm:text-xl font-semibold ">
           Let's discover your new favorite books !
         </p>
       </div>
 
       {/* SCROLL AREA */}
-      <div className="relative w-full max-w-full justify-center pl-[48px] pr-[32px] pb-[32px] pt-[48px] bg-[#faf4ef] rounded-[90px] overflow-x-auto scroll-smooth no-scrollbar">
-        {/* LEFT BUTTON */}
-        <button className="absolute left-10 top-1/2 -translate-y-1/2 z-10 text-7xl font-extrabold transform scale-y-200 text-[#878584] hover:text-black transition">
+      <div
+        className="relative w-full bg-[#faf4ef] rounded-[50px] px-6 sm:px-8 py-6"
+        item-center
+      >
+        <button
+          onClick={() => {
+            const cardWidth = scrollRef.current?.firstChild?.clientWidth || 0;
+
+            scrollRef.current?.scrollBy({
+              left: -cardWidth - 16,
+              behavior: "smooth",
+            });
+          }}
+          className="absolute left-4 top-1/2 -translate-y-1/2 z-10 text-3xl font-bold text-[#878584] hover:text-black transition"
+        >
           &lt;
         </button>
 
         {/* RIGHT BUTTON */}
-        <button className="absolute right-10 top-1/2 -translate-y-1/2 z-10 text-7xl font-extrabold transform scale-y-200 text-[#878584] hover:text-black transition">
+        <button
+          onClick={() => {
+            const cardWidth = scrollRef.current?.firstChild?.clientWidth || 0;
+
+            scrollRef.current?.scrollBy({
+              left: cardWidth + 16,
+              behavior: "smooth",
+            });
+          }}
+          className="absolute right-4 top-1/2 -translate-y-1/2 z-10 text-3xl font-bold text-[#878584] hover:text-black transition"
+        >
           &gt;
         </button>
 
         {/* BOOK LIST */}
-        <div className="flex flex-row gap-[72px] justify-center">
+        <div
+          ref={scrollRef}
+          className="flex gap-4 lg:gap-8 px-2 overflow-x-auto scrollbar-hide scroll-smooth snap-x snap-mandatory items-start lg:justify-center"
+        >
           {/* BOOK 1 */}
-          <div className="w-[160px] h-[240px] m-[8px]">
+          <div className="flex-none min-w-[85%] sm:min-w-[46%] lg:min-w-[27%] snap-start">
             <img
-              className="object-cover w-full h-full transition duration-300 hover:-translate-y-4 hover:shadow-[0_10px_20px_rgba(0,0,0,0.5)] hover:scale-105"
+              className="object-cover w-full aspect-[3/4] transition duration-300 hover:-translate-y-2 hover:shadow-[0_10px_20px_rgba(0,0,0,0.5)] hover:scale-105"
               src="./hero-section-image/hero-book-01.jpg"
               alt="Project Hail Mary"
             />
-            <div className="my-[16px] h-[100px] w-[300px]">
+            <div className="mt-4 w-full">
               <a
                 href="#"
-                className="font-extrabold text-2xl py-[8px] hover:underline"
+                className="font-extrabold text-lg sm:text-xl lg:text-2xl py-[8px] hover:underline [text-wrap:pretty]"
               >
                 Project Hail Mary
               </a>
               <div className="flex flex-row gap-2 items-center">
-                <p className="text-xl">4.7</p>
+                <p className="text-base sm:text-lg lg:text-xl">4.7</p>
                 <img className="w-4 h-4" src="./hero-section-image/star.png" />
                 <img className="w-4 h-4" src="./hero-section-image/star.png" />
                 <img className="w-4 h-4" src="./hero-section-image/star.png" />
@@ -52,23 +76,28 @@ export default function Hero() {
                   src="./hero-section-image/half-star.png"
                 />
               </div>
-              <p className="font-bold text-2xl">434.15 THB</p>
+              <p className="font-bold text-lg sm:text-xl lg:text-2xl">
+                434.15 THB
+              </p>
             </div>
           </div>
 
           {/* BOOK 2 */}
-          <div className="w-[160px] h-[240px] m-[8px]">
+          <div className="flex-none min-w-[85%] sm:min-w-[46%] lg:min-w-[27%] snap-start">
             <img
-              className="object-cover w-full h-full transition duration-300 hover:-translate-y-4 hover:shadow-[0_10px_20px_rgba(0,0,0,0.5)] hover:scale-105"
+              className="object-cover w-full aspect-[3/4] transition duration-300 hover:-translate-y-2 hover:shadow-[0_10px_20px_rgba(0,0,0,0.5)] hover:scale-105"
               src="./hero-section-image/hero-book-02.jpg"
               alt="The Nightingale"
             />
-            <div className="my-[16px] h-[100px] w-[300px]">
-              <a href="#" className="font-extrabold text-2xl hover:underline">
+            <div className="mt-4 w-full">
+              <a
+                href="#"
+                className="font-extrabold text-lg sm:text-xl lg:text-2xl py-[8px] hover:underline [text-wrap:pretty]"
+              >
                 The Nightingale
               </a>
               <div className="flex flex-row gap-2 items-center">
-                <p className="text-xl">4.8</p>
+                <p className="text-base sm:text-lg lg:text-xl">4.8</p>
                 <img className="w-4 h-4" src="./hero-section-image/star.png" />
                 <img className="w-4 h-4" src="./hero-section-image/star.png" />
                 <img className="w-4 h-4" src="./hero-section-image/star.png" />
@@ -78,23 +107,28 @@ export default function Hero() {
                   src="./hero-section-image/half-star.png"
                 />
               </div>
-              <p className="font-bold text-2xl">391.98 THB</p>
+              <p className="font-bold text-lg sm:text-xl lg:text-2xl">
+                391.98 THB
+              </p>
             </div>
           </div>
 
           {/* BOOK 3 */}
-          <div className="w-[160px] h-[240px] m-[8px]">
+          <div className="flex-none min-w-[85%] sm:min-w-[46%] lg:min-w-[27%] snap-start">
             <img
-              className="object-cover w-full h-full transition duration-300 hover:-translate-y-4 hover:shadow-[0_10px_20px_rgba(0,0,0,0.5)] hover:scale-105"
+              className="object-cover w-full aspect-[3/4] transition duration-300 hover:-translate-y-2 hover:shadow-[0_10px_20px_rgba(0,0,0,0.5)] hover:scale-105"
               src="./hero-section-image/hero-book-03.jpg"
               alt="The Housemaid"
             />
-            <div className="my-[16px] h-[100px] w-[300px]">
-              <a href="#" className="font-extrabold text-2xl hover:underline">
+            <div className="mt-4 w-full">
+              <a
+                href="#"
+                className="font-extrabold text-lg sm:text-xl lg:text-2xl py-[8px] hover:underline [text-wrap:pretty]"
+              >
                 The Housemaid
               </a>
               <div className="flex flex-row gap-2 items-center">
-                <p className="text-xl">4.5</p>
+                <p className="text-base sm:text-lg lg:text-xl">4.5</p>
                 <img className="w-4 h-4" src="./hero-section-image/star.png" />
                 <img className="w-4 h-4" src="./hero-section-image/star.png" />
                 <img className="w-4 h-4" src="./hero-section-image/star.png" />
@@ -104,7 +138,9 @@ export default function Hero() {
                   src="./hero-section-image/half-star.png"
                 />
               </div>
-              <p className="font-bold text-2xl">391.98 THB</p>
+              <p className="font-bold text-lg sm:text-xl lg:text-2xl">
+                391.98 THB
+              </p>
             </div>
           </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
-import {useState, useRef} from "react"
+import { useState, useRef } from "react";
 
-import NavBar from "../components/HomeComponents/NavBar"
+import NavBar from "../components/HomeComponents/NavBar";
 // import Hero from "../components/HomeComponents/Hero"
-import Banner from "../components/HomeComponents/Banner"
+import Hero from "../components/HomeComponents/Hero";
+import Banner from "../components/HomeComponents/Banner";
 // import CategorySample from "../components/HomeComponents/CategorySample";
 import Footer from "../components/HomeComponents/Footer";
 import PopupModal from "../components/PopupModal";
@@ -11,49 +12,52 @@ import { bookData } from "../mock-data/bookData";
 import { reviewData } from "../mock-data/reviewData";
 
 export default function Home() {
-    const [selectedCategory,setSelectredCategory] = useState("All");
-    const [selectedPayment,setSelectedPayment] = useState("QR");
-    const [cartItems,setCartItems] = useState([]);
-    const [likeItems,setLikeItems] = useState([]);
-    const [purchasedItems,setPurchasedItems] = useState([]);
-    const [isCartOpen,setIsCartOpen] = useState(false);
-    const [isShelfOpen,setIsShelfOpen] = useState(false);
-    const [isLogin,setIsLogin] = useState(false);
-    const [isRegister,setIsRegister] = useState(false);
-    const [isPaid,setIsPaid] = useState(false);
-    const [isConfirmedOrder,setIsconfirmedOrder] = useState(false);
-    const [isConfirmedPayment,setIsConfirmedPayment] = useState(false);
-    const [isUseCoupon,setUseCoupon] = useState(false);
-    const [isShowTextbox,setIsShowTextbox] = useState(false);
-    const [isAdmin,setIsAdmin] = useState(false);
-    const slideHeroRef = useRef(null);
-    const slideCategoryRef = useRef(null);
-    const slideLikeRef = useRef(null);
-    const slidePurchased = useRef(null);
-    
-    // for PopupModal (น่าจะต้องเปลี่ยนหรือเพิ่ม ถ้าต้องเรียกหลาย Popup)
-    const [open, setOpen] = useState(false);
-    
-    const CATEGORIES = ["All",...new Set(bookData.map((book) => book.category))]
-    
-    const activeCategory = bookData.filter((book) => selectedCategory === "All" || book.category === selectedCategory);
-    
-    const highlightBook = bookData.filter((book) => book.is_highlighted === "true");
-    
-    
-    const handleCategory = (newCategory) => {
-        setSelectredCategory(newCategory);
-    }
-    
+  const [selectedCategory, setSelectredCategory] = useState("All");
+  const [selectedPayment, setSelectedPayment] = useState("QR");
+  const [cartItems, setCartItems] = useState([]);
+  const [likeItems, setLikeItems] = useState([]);
+  const [purchasedItems, setPurchasedItems] = useState([]);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [isShelfOpen, setIsShelfOpen] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
+  const [isRegister, setIsRegister] = useState(false);
+  const [isPaid, setIsPaid] = useState(false);
+  const [isConfirmedOrder, setIsconfirmedOrder] = useState(false);
+  const [isConfirmedPayment, setIsConfirmedPayment] = useState(false);
+  const [isUseCoupon, setUseCoupon] = useState(false);
+  const [isShowTextbox, setIsShowTextbox] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const slideHeroRef = useRef(null);
+  const slideCategoryRef = useRef(null);
+  const slideLikeRef = useRef(null);
+  const slidePurchased = useRef(null);
+  const scrollRef = useRef(null);
 
+  // for PopupModal (น่าจะต้องเปลี่ยนหรือเพิ่ม ถ้าต้องเรียกหลาย Popup)
+  const [open, setOpen] = useState(false);
 
-    return (
-        <>
-            <NavBar />
-            {/* <Hero /> */}
-            <Banner />
-            {/* <CategorySample /> */}
-            <Footer />
-        </>
-    );
+  const CATEGORIES = ["All", ...new Set(bookData.map((book) => book.category))];
+
+  const activeCategory = bookData.filter(
+    (book) => selectedCategory === "All" || book.category === selectedCategory,
+  );
+
+  const highlightBook = bookData.filter(
+    (book) => book.is_highlighted === "true",
+  );
+
+  const handleCategory = (newCategory) => {
+    setSelectredCategory(newCategory);
+  };
+
+  return (
+    <>
+      <NavBar />
+      {/* <Hero /> */}
+      <Hero />
+      <Banner />
+      {/* <CategorySample /> */}
+      <Footer />
+    </>
+  );
 }
